019e529485f6a5505cfd572276f91ad78237268a
IngvarrFX <nurov.mj@gmail.com>
2022-06-14 17:00:04 +0300

    add children props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 
 type PropsType = {
     title: string
     className?: "primary" | "secondary" | "success" | "add" | "remove" | "edit" | "close"
     onClick: () => void
+    children?:React.ReactNode
 }
 
 export const Button = (props: PropsType) => {
-    const {title, className, onClick} = props;
-    return (<button className={className ? styles[className] : styles.primary} onClick={onClick}>{title}</button>
+    const {title, className, onClick, children} = props;
+    return (<button className={className ? styles[className] : styles.primary} onClick={onClick}>
+            {children}
+            {title}
+    </button>
     );
 };
