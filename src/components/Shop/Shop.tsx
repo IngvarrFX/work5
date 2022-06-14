acacc8ee85632c07b0970a9770ddffaad73a72ba
IngvarrFX <nurov.mj@gmail.com>
2022-06-14 17:05:46 +0300

    refactor styles and add svg components for button

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Shop.module.css";
 import {ShopType} from "../../Context/appContext";
 import {Button} from "../Button";
+import {ReactComponent as Cancel} from "../../icons/cancel.svg";
+import {ReactComponent as Edit} from "../../icons/pencil.svg";
 
 type ShopPropsType = {
     shop: ShopType
@@ -9,16 +11,24 @@ type ShopPropsType = {
 
 export const Shop = (props: ShopPropsType) => {
     const {shop} = props;
-    return (<tr className={styles.row}>
-            <td>{shop.number}</td>
-            <td>{shop.name}</td>
-            <td>{shop.address}</td>
-            <td>
-                <div className={styles.btn}>
-                    <Button title={"Редактировать"} className={"edit"} onClick={()=>{}}/>
-                    <Button title={"Редактировать"} className={"remove"} onClick={()=>{}}/>
-                </div>
-            </td>
-        </tr>
+    return (<>
+            <tr className={styles.row}>
+                <td>{shop.number}</td>
+                <td>{shop.name}</td>
+                <td>{shop.address}</td>
+                <td>
+                    <div className={styles.btn}>
+                        <Button title={"Редактировать"} className={"edit"} onClick={() => {
+                        }}>
+                            <Edit/>
+                        </Button>
+                        <Button title={"Редактировать"} className={"remove"} onClick={() => {
+                        }}>
+                            <Cancel/>
+                        </Button>
+                    </div>
+                </td>
+            </tr>
+        </>
     );
 };
